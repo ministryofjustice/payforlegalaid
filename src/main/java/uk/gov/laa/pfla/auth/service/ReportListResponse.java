d75a030313eabcdb6cbfbcf05b6157f532ce4d56
Luke J <134690131+Lukehub1@users.noreply.github.com>
2023-08-08 18:57:24 +0100

    Addressing PR comments, adding fields and adding model + response classes

---
+++ src/main/java/uk/gov/laa/pfla/auth/service/ReportListResponse.java
@@ -0,0 +1,36 @@
+package uk.gov.laa.pfla.auth.service;
+
+import lombok.Data;
+
+
+/**
+ * A class defining a /reports endpoint response object. This response consists of a list of data about a single report.
+ * Multiple response objects are sent to the user, forming a list of reports in JSON format
+ */
+@Data
+public class ReportListResponse {
+
+    private int id;
+    private String reportName;
+    private String reportPeriod;
+    private String reportOwner;
+    private String reportCreator;
+    private String description;
+    private String baseUrl;
+
+
+    public ReportListResponse(int id, String reportName, String reportPeriod, String reportOwner, String reportCreator, String reportDescription, String baseUrl) {
+        this.id = id;
+        this.reportName = reportName;
+        this.reportPeriod = reportPeriod;
+        this.reportOwner = reportOwner;
+        this.reportCreator = reportCreator;
+        this.description = reportDescription;
+        this.baseUrl = baseUrl;
+
+    }
+
+
+
+
+}
