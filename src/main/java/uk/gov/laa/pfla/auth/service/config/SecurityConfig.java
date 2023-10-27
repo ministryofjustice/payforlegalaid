5d3dba7a0dd1d95b3ecc716528f2b7fff33981da
Luke J <134690131+Lukehub1@users.noreply.github.com>
2023-10-27 16:26:27 +0100

    Testing AWS Secrets with code branch from before SSO classes were added

--- src/main/java/uk/gov/laa/pfla/auth/service/config/SecurityConfig.java
+++ src/main/java/uk/gov/laa/pfla/auth/service/config/SecurityConfig.java
@@ -1,42 +1,42 @@
 // todo: uncomment class
-// package uk.gov.laa.pfla.auth.service.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//import com.azure.core.client.traits.HttpTrait;
-//
-////@EnableWebSecurity
-////@EnableGlobalMethodSecurity(prePostEnabled = true)
-//@Configuration
-//public class SecurityConfig {
-//
-//  @Bean
-//  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-//
-//
-//    return http
-//            // you probably want a request matcher since you are using @Order
-//            .authorizeHttpRequests(authorize -> authorize
-//                    .anyRequest().authenticated()
-//            ).build();
-//
-//
-//
-////     return http
-////          .authorizeHttpRequests()
-////  //        .antMatchers("/ssologout.do").permitAll()
-////  //        .antMatchers("/logout").denyAll()
-////          .anyRequest()
-////          .authenticated()
-////          .build();
-//  //        .and()
-//  //        .logout()
-//  //        .logoutUrl("/ssologout.do")
-//  //        .logoutSuccessUrl("/")
-//  //        .permitAll();
-//    }
-//}
+ package uk.gov.laa.pfla.auth.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import com.azure.core.client.traits.HttpTrait;
+
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+
+    return http
+            // you probably want a request matcher since you are using @Order
+            .authorizeHttpRequests(authorize -> authorize
+                    .anyRequest().authenticated()
+            ).build();
+
+
+
+//     return http
+//          .authorizeHttpRequests()
+//  //        .antMatchers("/ssologout.do").permitAll()
+//  //        .antMatchers("/logout").denyAll()
+//          .anyRequest()
+//          .authenticated()
+//          .build();
+  //        .and()
+  //        .logout()
+  //        .logoutUrl("/ssologout.do")
+  //        .logoutSuccessUrl("/")
+  //        .permitAll();
+    }
+}
