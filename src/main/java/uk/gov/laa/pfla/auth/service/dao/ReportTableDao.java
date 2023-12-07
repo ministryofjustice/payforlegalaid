bc038982de84e8726f076d8c27d093e8a09c7edb
Luke J <134690131+Lukehub1@users.noreply.github.com>
2023-12-07 16:15:52 +0000

    pulling with hardcoded sql - report from a hardcoded view

--- src/main/java/uk/gov/laa/pfla/auth/service/dao/ReportTableDao.java
+++ src/main/java/uk/gov/laa/pfla/auth/service/dao/ReportTableDao.java
@@ -1,27 +1,64 @@
 package uk.gov.laa.pfla.auth.service.dao;
 
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.MappingException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import uk.gov.laa.pfla.auth.service.builders.ReportTableModelBuilder;
 import uk.gov.laa.pfla.auth.service.models.ReportTableModel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-import java.time.LocalDateTime;
 @Repository
+@Slf4j
 public class ReportTableDao {
 
 
-//    private final JdbcTemplate jdbcTemplate; //pass this into the constructor as an arg once it's ready?
+    private final List<ReportTableModel> reportTableObjectList = new ArrayList<>();
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private final ModelMapper mapper = new ModelMapper();
+
+    public List<ReportTableModel> fetchReport(int requestedId) {
+
+        reportTableObjectList.clear(); // Prevent data accumulating after multiple requests
+
+        List<Map<String, Object>> resultList;
+
+        String query = "SELECT * FROM ANY_REPORT.V_BANK_MONTH";
 
 
-    public ReportTableModel fetchReport(int requestedId) {
-        LocalDateTime placeHolderDateTime = LocalDateTime.now();
+        resultList = jdbcTemplate.queryForList(query);
+        log.info("Result list, a list of objects each representing a row in the DB: " + resultList);
+
+        try {
+            resultList.forEach(obj -> {
+                ReportTableModel reportTableObject = mapper.map(obj, ReportTableModel.class);
+                reportTableObjectList.add(reportTableObject);
+            });
+        } catch (MappingException e) {
+            log.error("Exception with model map loop: " + e);
+        }
+
+
+        return reportTableObjectList;
 
 
 
-        return new ReportTableModelBuilder()
-                .withId(requestedId).withReportName("AP_and_AR_Combined-DEBT-AGING-SUMMARY-4")
-                .withReportUrl("www.sharepoint.com/an-example-report.csv")
-                .withCreationTime(placeHolderDateTime)
-                .createReportModel();
+
+//        LocalDateTime placeHolderDateTime = LocalDateTime.now();
+//
+//
+//
+//        return new ReportTableModelBuilder()
+//                .withId(requestedId).withReportName("AP_and_AR_Combined-DEBT-AGING-SUMMARY-4")
+//                .withReportUrl("www.sharepoint.com/an-example-report.csv")
+//                .withCreationTime(placeHolderDateTime)
+//                .createReportModel();
 
     }
 
--- src/main/java/uk/gov/laa/pfla/auth/service/services/ReportService.java
+++ src/main/java/uk/gov/laa/pfla/auth/service/services/ReportService.java
@@ -3,9 +3,12 @@ package uk.gov.laa.pfla.auth.service.services;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import uk.gov.laa.pfla.auth.service.dao.ReportTableDao;
+import uk.gov.laa.pfla.auth.service.models.MappingTableModel;
 import uk.gov.laa.pfla.auth.service.models.ReportTableModel;
 import uk.gov.laa.pfla.auth.service.responses.ReportResponse;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -16,18 +19,31 @@ public class ReportService {
 
     private final ReportTableDao reportTableDao;
 
+    private final List<ReportResponse> reportResponses = new ArrayList<>();
+
     public ReportService(ReportTableDao reportTableDao) {
         this.reportTableDao = reportTableDao;
     }
 
     public ReportResponse createReportResponse(int id) {
 
-        //Fetching reportList items from database
-        ReportTableModel reportTableObject = reportTableDao.fetchReport(id);
 
-        ReportResponse reportResponse = mapper.map(reportTableObject, ReportResponse.class);
 
-        return reportResponse;
+
+        //Fetching report items from database
+        List<ReportTableModel> reportTableObjectList = reportTableDao.fetchReport(id);
+
+        reportTableObjectList.forEach(reportTableObject -> {
+
+
+            ReportResponse reportResponse = mapper.map(reportTableObject, ReportResponse.class);
+
+            reportResponses.add(reportResponse);
+
+        });
+
+        return reportResponses.get(0);
+
     }
 
 
