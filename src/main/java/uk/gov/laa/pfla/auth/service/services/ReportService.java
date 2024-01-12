87a88e4802fe7ced36ca09cd9f7a5116bc78b528
Luke J <134690131+Lukehub1@users.noreply.github.com>
2024-01-12 14:30:31 +0000

    First attempt at csv + test

--- src/main/java/uk/gov/laa/pfla/auth/service/services/ReportService.java
+++ src/main/java/uk/gov/laa/pfla/auth/service/services/ReportService.java
@@ -1,5 +1,7 @@
 package uk.gov.laa.pfla.auth.service.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.springframework.stereotype.Service;
 import uk.gov.laa.pfla.auth.service.dao.ReportViewsDao;
 import uk.gov.laa.pfla.auth.service.models.report_view_models.ReportModel;
@@ -7,10 +9,18 @@ import uk.gov.laa.pfla.auth.service.models.report_view_models.VBankMonth;
 import uk.gov.laa.pfla.auth.service.models.report_view_models.VCisToCcmsInvoiceSummaryModel;
 import uk.gov.laa.pfla.auth.service.responses.ReportResponse;
 import uk.gov.laa.pfla.auth.service.responses.ReportListResponse;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -45,17 +55,13 @@ public class ReportService {
         }else{ throw new IndexOutOfBoundsException("Report ID needs to be a number between 0 and 1000");}
 
         Class<? extends ReportModel> classOne = reportModelMapping.get(id);
-        //Fetch report data from MOJFIN database
-        List<ReportModel> reportViewObjectList = fetchReportViewObjectList(classOne, reportListResponse.getSqlQuery());
+        //Fetch fetchReportViewObjectList data from MOJFIN database
+//        List<ReportModel> reportViewObjectList = fetchReportViewObjectList(classOne, reportListResponse.getSqlQuery());
 
 
         // Create csv here
-//        List<String> collect = reportViewObjectList.stream().map(new Function<ReportModel, String>() {
-//            @Override
-//            public String apply(ReportModel reportModel) {
-//                return reportModel.asCSV();
-//            }
-//        }).collect(Collectors.toList());
+        List<Map<String, Object>> resultList = reportViewsDao.callDataBase(reportListResponse.getSqlQuery());
+        convertToCSV(resultList);
 
         ReportResponse reportResponse = new ReportResponse();
         reportResponse.setId(reportListResponse.getId());
@@ -74,12 +80,50 @@ public class ReportService {
         //Use the id from the customer's request to define the report model we need to use (when we later query the database)
 
         //Fetching report items from database report views (using the SQL query string from the mapping table)
-        List<ReportModel> reportViewObjectList = reportViewsDao.fetchReport(sqlQuery, clazz);
+        List<ReportModel> reportViewObjectList = reportViewsDao.fetchReportViewObjectList(sqlQuery, clazz);
 
         log.debug("Object table list size: {}", reportViewObjectList.size()); // Checking if the list is unexpectedly empty
 
         return reportViewObjectList;
     }
+
+    public void convertToCSV(List<Map<String, Object>> resultList) {
+
+
+//        StringWriter sw = new StringWriter();
+        FileWriter out = null;
+        try {
+            out = new FileWriter("book_new.csv");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        CSVFormat csvFormat = CSVFormat.ORACLE.builder()
+//                .setHeader(HEADERS)
+                .build();
+
+        try {
+//            final CSVPrinter printer = CSVFormat.ORACLE.withHeader(resultList).print(out);
+            final CSVPrinter printer = new CSVPrinter(out, csvFormat);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+//        CSVFormat csvFormat = CSVFormat.ORACLE.builder()
+//                .setHeader(HEADERS)
+//                .build();
+
+
+
+
+//        return Stream.of(list)
+//                .map(this::escapeSpecialCharacters)
+//                .collect(Collectors.joining(","));
+    }
+
 
 
 }
--- src/test/java/uk/gov/laa/pfla/auth/service/service/ReportServiceTest.java
+++ src/test/java/uk/gov/laa/pfla/auth/service/service/ReportServiceTest.java
@@ -1,5 +1,9 @@
 package uk.gov.laa.pfla.auth.service.service;
 
+import org.apache.commons.csv.CSVFormat;
+import org.json.*;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,12 +15,13 @@ import uk.gov.laa.pfla.auth.service.models.report_view_models.VBankMonth;
 import uk.gov.laa.pfla.auth.service.services.MappingTableService;
 import uk.gov.laa.pfla.auth.service.services.ReportService;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,6 +35,43 @@ public class ReportServiceTest {
     @InjectMocks
     ReportService reportService;
 
+    Map<String, Object> authorBookMap;
+    List<Map<String, Object>> authorBookMapList = new ArrayList<>();
+    @BeforeEach
+    void init() {
+        JSONObject json1;
+
+//        LOG.info("startup");
+        try {
+
+            json1 = new JSONObject("{\"name\":\"John\", \"age\":30, \"car\":null}");
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        authorBookMap = Collections.unmodifiableMap(new LinkedHashMap<String, Object>() {
+
+
+            {
+                put("Dan Simmons", json1);
+                put("Douglas Adams", json1);
+            }
+        });
+
+        authorBookMapList.add(authorBookMap);
+
+    }
+
+
+
+
+    public static final String[] HEADERS = { "author", "title" };
+
+    enum BookHeaders{
+        author, title
+    }
+
+    public static final String EXPECTED_FILESTREAM = "author,title\r\n" + "Dan Simmons,Hyperion\r\n" + "Douglas Adams,The Hitchhiker's Guide to the Galaxy";
 
     @Test
     public void fetchReportViewObjectList_ReturnsCorrectReportModelType(){
@@ -56,17 +98,46 @@ public class ReportServiceTest {
         expectedReportViewObjectList.add(expectedReportViewObject);
 
         // Simulate report model and service behavior
-        when(reportViewsDAO.fetchReport(sqlQuery,VBankMonth.class)).thenReturn(expectedReportViewObjectList);
+        when(reportViewsDAO.fetchReportViewObjectList(sqlQuery,VBankMonth.class)).thenReturn(expectedReportViewObjectList);
 
 
         // Act
         List<ReportModel> actualReportViewObjectList = reportService.fetchReportViewObjectList(VBankMonth.class, sqlQuery);
 
         // Assert
-        verify(reportViewsDAO, times(1)).fetchReport(sqlQuery,VBankMonth.class);
+        verify(reportViewsDAO, times(1)).fetchReportViewObjectList(sqlQuery,VBankMonth.class);
         assertEquals(1, actualReportViewObjectList.size());
         assertEquals(expectedReportViewObject, actualReportViewObjectList.get(0));
         assertThat(actualReportViewObjectList.get(0), instanceOf(VBankMonth.class));
 
     }
+
+
+    @Test
+    void givenAuthorBookMap_whenWrittenToStream_thenOutputStreamAsExpected() throws IOException {
+        StringWriter sw = new StringWriter();
+        FileWriter out = new FileWriter("book_new.csv");
+
+
+        CSVFormat csvFormat = CSVFormat.ORACLE.builder()
+                .setHeader(HEADERS)
+                .build();
+
+        reportService.convertToCSV(authorBookMapList);
+//        try (CSVPrinter printer = new CSVPrinter(out, csvFormat)) {
+//            AUTHOR_BOOK_MAP.forEach((author, title) -> {
+//                try {
+//                    printer.printRecord(author, title);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            });
+//
+//        }
+
+
+//        assertEquals(EXPECTED_FILESTREAM, out.toString().trim());
+    }
+
+
 }
