9215dc5114c283076d21cb4b02cd8346e15dc575
Luke J <134690131+Lukehub1@users.noreply.github.com>
2024-02-29 10:23:57 +0000

    Basic call to report tracking table, trying more hardcoded syntax

--- src/main/java/uk/gov/laa/pfla/auth/service/dao/ReportTrackingTableDao.java
+++ src/main/java/uk/gov/laa/pfla/auth/service/dao/ReportTrackingTableDao.java
@@ -37,30 +37,42 @@ public class ReportTrackingTableDao {
 //                trackingModel.getCreationTime(), trackingModel.getMappingId(), trackingModel.getReportGeneratedBy());
 
 
+        String sql = "INSERT INTO GPFD.REPORT_TRACKING (ID, REPORT_NAME, REPORT_URL, CREATION_TIME, MAPPING_ID, REPORT_GENERATED_BY) VALUES (?,?,?,?,?,?)";
+
+        //Insert values into sql statement and update
+        jdbcTemplate.update(sql, 1, "Test name 1", "www.test-site.com",
+                trackingModel.getCreationTime(), 2, "Tony Soprano");
+
+
+
 //        String sql = "INSERT INTO GPFD.REPORT_TRACKING (ID, REPORT_NAME, REPORT_URL, CREATION_TIME, MAPPING_ID, REPORT_GENERATED_BY) VALUES (1, 'test report name 2', 'www.sharepoint.com/place-where-we-will-create-report', null, 2, 'Liv Tyler')";
 
-
-//        PreparedStatement stmt = "INSERT INTO GPFD.REPORT_TRACKING (ID, REPORT_NAME, REPORT_URL, CREATION_TIME, MAPPING_ID, REPORT_GENERATED_BY) VALUES (1, 'test report name 2', 'www.sharepoint.com/place-where-we-will-create-report', null, 2, 'Liv Tyler')";
-//        //Insert values into sql statement and update
+        //Insert values into sql statement and update
 //        jdbcTemplate.update(sql);
 //
+
 //        this.jdbcTemplate.update(
 //                "INSERT INTO GPFD.REPORT_TRACKING (ID, REPORT_NAME, REPORT_URL, CREATION_TIME, MAPPING_ID, REPORT_GENERATED_BY) VALUES (?,?,?,?,?,?)",
 //                trackingModel.getId(), trackingModel.getReportName(), trackingModel.getReportUrl(),
 //                null, trackingModel.getMappingId(), trackingModel.getReportGeneratedBy());
 
-        SimpleJdbcInsert simpleJdbcInsert =
-                new SimpleJdbcInsert(jdbcTemplate).withTableName("REPORT_TRACKING");
 
-        Map<String,Object> parameters = new HashMap<>();
-        parameters.put("ID",trackingModel.getId());
-        parameters.put("REPORT_NAME",trackingModel.getReportName());
-        parameters.put("REPORT_URL",trackingModel.getReportUrl());
-        parameters.put("CREATION_TIME",trackingModel.getCreationTime());
-        parameters.put("MAPPING_ID",trackingModel.getMappingId());
-        parameters.put("REPORT_GENERATED_BY",trackingModel.getReportGeneratedBy());
 
-        simpleJdbcInsert.execute(parameters);
+
+
+
+//        SimpleJdbcInsert simpleJdbcInsert =
+//                new SimpleJdbcInsert(jdbcTemplate).withTableName("REPORT_TRACKING");
+//
+//        Map<String,Object> parameters = new HashMap<>();
+//        parameters.put("ID",trackingModel.getId());
+//        parameters.put("REPORT_NAME",trackingModel.getReportName());
+//        parameters.put("REPORT_URL",trackingModel.getReportUrl());
+//        parameters.put("CREATION_TIME",trackingModel.getCreationTime());
+//        parameters.put("MAPPING_ID",trackingModel.getMappingId());
+//        parameters.put("REPORT_GENERATED_BY",trackingModel.getReportGeneratedBy());
+//
+//        simpleJdbcInsert.execute(parameters);
 
     }
 
