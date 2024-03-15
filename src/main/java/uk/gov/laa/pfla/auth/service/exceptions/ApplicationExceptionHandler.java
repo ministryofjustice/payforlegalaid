02cfbc136894a413010b1bc49df325f5642969b1
Luke J <134690131+Lukehub1@users.noreply.github.com>
2024-03-15 11:25:34 +0000

    Adding status codes into error handler to make the class layout clearer

--- src/main/java/uk/gov/laa/pfla/auth/service/exceptions/ApplicationExceptionHandler.java
+++ src/main/java/uk/gov/laa/pfla/auth/service/exceptions/ApplicationExceptionHandler.java
@@ -14,7 +14,7 @@ import java.util.Map;
 public class ApplicationExceptionHandler {
 
     private static final String ERROR_STRING = "Error: ";
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 500
     @ExceptionHandler(CsvStreamException.class)
     public ResponseEntity<Map<String, String>> handleCsvStreamException(CsvStreamException e){
         Map<String, String> errorMap = new HashMap<>();
@@ -23,7 +23,7 @@ public class ApplicationExceptionHandler {
         return new ResponseEntity<>(errorMap, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 500
     @ExceptionHandler(DatabaseReadException.class)
     public ResponseEntity<Map<String, String>> handleDatabaseReadException(DatabaseReadException e){
         Map<String, String> errorMap = new HashMap<>();
@@ -33,7 +33,7 @@ public class ApplicationExceptionHandler {
 
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.BAD_REQUEST) //400
     @ExceptionHandler(ReportIdNotFoundException.class)
     public ResponseEntity<Map<String, String>>  handleReportIdNotFoundException(ReportIdNotFoundException e){
         Map<String, String> errorMap = new HashMap<>();
@@ -43,7 +43,7 @@ public class ApplicationExceptionHandler {
 
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.BAD_REQUEST) // 400
     @ExceptionHandler(IndexOutOfBoundsException.class)
     public ResponseEntity<Map<String, String>> handleIndexOutOfBoundsException(IndexOutOfBoundsException e){
         Map<String, String> errorMap = new HashMap<>();
@@ -52,7 +52,7 @@ public class ApplicationExceptionHandler {
         return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
 
     }
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.BAD_REQUEST) // 400
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<Map<String, String>> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e){
         String message = ERROR_STRING + "Invalid input for parameter " + e.getName() + ". Expected a numeric value";
