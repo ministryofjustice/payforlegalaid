d11172f88f67158de699cdc4cc7b99ef744ccd07
Robert Buczek <robert.buczek@digital.justice.gov.uk>
2024-11-21 10:57:11 +0000

    [LPF-511]: Update project package to `uk.gov.laa.gpfd`

---
+++ src/main/java/uk/gov/laa/gpfd/dao/MappingTableDao.java
@@ -0,0 +1,72 @@
+package uk.gov.laa.gpfd.dao;
+
+import org.modelmapper.MappingException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import uk.gov.laa.gpfd.exceptions.DatabaseReadException;
+import uk.gov.laa.gpfd.models.MappingTableModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Repository
+public class MappingTableDao {
+
+    private final List<MappingTableModel> mappingTableObjectList = new ArrayList<>();
+
+    public static final Logger log = LoggerFactory.getLogger(MappingTableDao.class);
+
+
+    private final JdbcTemplate readOnlyJdbcTemplate;
+    private final ModelMapper mapper = new ModelMapper();
+
+    // Using the JDBCTemplate bean defined in  PflaApplication (the @SpringBootApplication / run class) which uses the
+    //read only DB datasource/credentials
+    @Autowired
+    public MappingTableDao(JdbcTemplate readOnlyJdbcTemplate) {
+        this.readOnlyJdbcTemplate = readOnlyJdbcTemplate;
+    }
+
+
+    public List<MappingTableModel> fetchReportList() throws DatabaseReadException {
+        mappingTableObjectList.clear(); // Prevent data accumulating after multiple requests
+
+        List<Map<String, Object>> resultList;
+
+        String query = "SELECT * FROM GPFD.CSV_TO_SQL_MAPPING_TABLE";
+
+
+        try {
+            log.info("Reading mapping data");
+            resultList = readOnlyJdbcTemplate.queryForList(query);
+        } catch (DataAccessException e) {
+            throw new DatabaseReadException("Error reading from DB: " + e);
+        }
+
+        if (resultList.isEmpty()) {
+            throw new DatabaseReadException("No results returned from mapping table");
+        }
+
+
+        try {
+            resultList.forEach(obj -> {
+                MappingTableModel mappingTableObject = mapper.map(obj, MappingTableModel.class);
+                mappingTableObjectList.add(mappingTableObject);
+            });
+        } catch (MappingException e) {
+            log.error("Exception with model map loop: %s", e);
+        }
+
+
+        return mappingTableObjectList;
+
+
+    }
+}
