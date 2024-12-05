123de0e21f1120a0944e8682c05ea4dcb158ff34
christopher.cook <christopher.cook@digital.justice.gov.uk>
2024-12-05 09:48:03 +0000

    LPF-193 - cleanup imports

--- src/test/java/uk/gov/laa/gpfd/config/builders/HttpSecuritySessionManagementConfigurerBuilderLocalTest.java
+++ src/test/java/uk/gov/laa/gpfd/config/builders/HttpSecuritySessionManagementConfigurerBuilderLocalTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import uk.gov.laa.gpfd.builders.ReportResponseTestBuilder;
 import uk.gov.laa.gpfd.services.ReportService;
 
@@ -18,7 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ActiveProfiles("local")
 @AutoConfigureMockMvc
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = ("spring.h2.console.enabled=true"))
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HttpSecuritySessionManagementConfigurerBuilderLocalTest {
 
     @MockBean
