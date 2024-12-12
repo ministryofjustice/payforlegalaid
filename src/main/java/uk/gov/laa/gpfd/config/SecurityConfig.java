5871f7ceedfafeec3ca31c6a6a24bb8f3d25b489
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2024-12-12 18:33:13 +0000

    Adding oidc login & logout

--- src/main/java/uk/gov/laa/gpfd/config/SecurityConfig.java
+++ src/main/java/uk/gov/laa/gpfd/config/SecurityConfig.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+
 import uk.gov.laa.gpfd.config.builders.AuthorizeHttpRequestsBuilder;
 import uk.gov.laa.gpfd.config.builders.SessionManagementConfigurerBuilder;
 
@@ -24,6 +29,7 @@ import uk.gov.laa.gpfd.config.builders.SessionManagementConfigurerBuilder;
 @ConditionalOnProperty(name = "spring.cloud.azure.active-directory.enabled", havingValue = "true")
 public class SecurityConfig {
 
+    private final ClientRegistrationRepository clientRegistrationRepository;
     /**
      * The custom {@link AuthorizeHttpRequestsBuilder} responsible for configuring
      * the authorization rules for HTTP requests, such as which endpoints are publicly
@@ -53,12 +59,31 @@ public class SecurityConfig {
      * @return a configured {@link SecurityFilterChain} object.
      * @throws Exception if any error occurs during the configuration of HTTP security.
      */
+
+
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .authorizeHttpRequests(authorizeHttpRequestsBuilder)    // Apply authorization rules
-                .sessionManagement(sessionManagementConfigurerBuilder)  // Apply session management configuration
-                .build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorizeHttpRequestsBuilder::customize)
+            .sessionManagement(sessionManagementConfigurerBuilder::customize)
+            .oauth2Login(oauth2LoginConfigurer ->
+                oauth2LoginConfigurer.userInfoEndpoint(userInfoEndpointConfigurer ->
+                    userInfoEndpointConfigurer.oidcUserService(oidcUserService())
+                )
+            )
+            .logout(logoutConfigurer -> logoutConfigurer.logoutSuccessHandler(logoutConfigurer()));
+
+        return http.build();
     }
 
+        private OidcUserService oidcUserService(){
+        return new OidcUserService();
+    }
+
+    private LogoutSuccessHandler logoutConfigurer() {
+        OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler =
+            new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/");
+        return oidcLogoutSuccessHandler;
+    }
 }
