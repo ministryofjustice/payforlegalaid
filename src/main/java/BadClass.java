d31dce42ca0d3bf79b42b9a4c860fb448e405ccd
christopher.cook <christopher.cook@digital.justice.gov.uk>
2024-12-17 15:03:31 +0000

    LPF-514 - intentiaonlly bad code to test sonarcloud is blocking PRs that fail

--- src/main/java/BadClass.java
+++ src/main/java/BadClass.java
@@ -2,7 +2,6 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class BadClass {
 
     public static void main(String[] args) {
