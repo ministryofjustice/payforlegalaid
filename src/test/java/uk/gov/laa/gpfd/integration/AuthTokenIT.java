039405aa455ed6517549c0bfd211413d8558932c
SineadCummings <sinead.cummings@justice.gov.uk>
2024-12-18 10:20:29 +0000

    LPF-540 pt 2: Add integration tests for auth scenarios (#83)
    * LPF-537: Add IT test for /reports
    * Update exception called when no data found to NOT FOUND in place of bad request
    * Add extra test profile to allow running of tests both with (testauth.yml) and without auth (test.yml)
    * Update existing tests that require auth to use testauth profile
    * Add mapping table and test data to .sql files
    
    * LPF-537: Add IT test for /reports
    * Add test for 401 response
    * Add test for 500 response
    
    * LPF-538: Add IT test for /report/1
    * Add tests for 200, 400 and 404 response
    * Add to test class for 401 response
    * Add to test class for 500 response
    
    * LPF-538: correct method name
    
    * LPF-540: Get CSV by Id Integration tests
    * Currently failing on auth
    
    * LPF-540: Integration tests for getting csv from csv/{id} endpoint
    
    * LPF-540: Remove unused imports
    
    * LPF-540: add tests to confirm that user redirected to azure login when no auth token provided, and request is successful when auth token is provided
    
    * LPF-540: split auth tests into separate PR
    
    * LPF-540: updates post code review
    
    * LPF-540: updates post code review
    
    * LPF-540: Auth tests for all endpoints
    
    * LPF-540: Reformat file
    
    * LPF-540: Replace duplicates with parameterised tests
    
    * LPF-540: Update names of parameterised tests

---
+++ src/test/java/uk/gov/laa/gpfd/integration/AuthTokenIT.java
@@ -0,0 +1,99 @@
+package uk.gov.laa.gpfd.integration;
+
+import com.microsoft.graph.models.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import uk.gov.laa.gpfd.graph.AzureGraphClient;
+import uk.gov.laa.gpfd.utils.FileUtils;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oauth2Client;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@AutoConfigureMockMvc
+@ActiveProfiles("testauth")
+@TestPropertySource(locations = "classpath:application-testauth.yml")
+@SpringBootTest
+class AuthTokenIT {
+
+    @MockBean
+    AzureGraphClient mockAzureGraphClient;
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private JdbcTemplate writeJdbcTemplate;
+
+    @BeforeEach
+    void setupDatabase() {
+        String gpfdSqlSchema = FileUtils.readResourceToString("gpfd_schema.sql");
+        String gpfdSqlData = FileUtils.readResourceToString("gpfd_data.sql");
+        writeJdbcTemplate.execute(gpfdSqlSchema);
+        writeJdbcTemplate.execute(gpfdSqlData);
+
+        String anyReportSqlSchema = FileUtils.readResourceToString("any_report_schema.sql");
+        String anyReportSqlData = FileUtils.readResourceToString("any_report_data.sql");
+        writeJdbcTemplate.execute(anyReportSqlSchema);
+        writeJdbcTemplate.execute(anyReportSqlData);
+    }
+
+    @AfterEach
+    void resetDatabase() {
+        writeJdbcTemplate.update("TRUNCATE TABLE GPFD.REPORT_TRACKING");
+        writeJdbcTemplate.update("DROP SEQUENCE GPFD_TRACKING_TABLE_SEQUENCE");
+        writeJdbcTemplate.update("TRUNCATE TABLE GPFD.CSV_TO_SQL_MAPPING_TABLE");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"/reports", "/reports/1", "/csv/1"})
+    void shouldRedirectToLoginWithoutAuthToken(String endpoint) throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(get(endpoint)
+                .contentType(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+        Assertions.assertEquals(302, response.getStatus());
+        Assertions.assertTrue(response.getRedirectedUrl().contains("/oauth2/authorization/azure"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"/reports", "/reports/1"})
+    void shouldReturn200WhenLoginAuthTokenProvided(String endpoint) throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(get(endpoint)
+                .with(SecurityMockMvcRequestPostProcessors.oauth2Login())
+                .contentType(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+        Assertions.assertEquals(200, response.getStatus());
+    }
+
+    @Test
+    void getCsvWithIdShouldReturn200WhenLoginAuthTokenProvided() throws Exception {
+        User user = new User();
+        user.userPrincipalName = "Test User";
+
+        when(mockAzureGraphClient.getGraphUserDetails(any())).thenReturn(user);
+
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.get("/csv/1")
+                .with(SecurityMockMvcRequestPostProcessors.oauth2Login())
+                .with(oidcLogin()
+                        .idToken(token -> token.subject("mockUser")))
+                .with(oauth2Client("graph"))).andReturn().getResponse();
+
+        Assertions.assertEquals(200, response.getStatus());
+    }
+}
