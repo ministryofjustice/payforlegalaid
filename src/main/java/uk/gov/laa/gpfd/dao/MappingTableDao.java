ba48b2370c61f0b1ce1450239e119dadf261dcd8
jason.smallman <jason.smallman@digital.justice.gov.uk>
2025-01-09 13:44:46 +0000

    added parameters for queries Signed-off-by: jason.smallman <jason.smallman@digital.justice.gov.uk>

--- src/main/java/uk/gov/laa/gpfd/dao/MappingTableDao.java
+++ src/main/java/uk/gov/laa/gpfd/dao/MappingTableDao.java
@@ -20,18 +20,25 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class MappingTableDao {
     private static final String SELECT_SQL = "SELECT * FROM GPFD.CSV_TO_SQL_MAPPING_TABLE ORDER BY ID ASC";
-    private static final String SELECT_SINGLE_ITEM_SQL = "SELECT * FROM GPFD.CSV_TO_SQL_MAPPING_TABLE WHERE ID = ";
+    private static final String SELECT_SINGLE_ITEM_SQL = "SELECT * FROM GPFD.CSV_TO_SQL_MAPPING_TABLE WHERE ID = ?";
 
     private final JdbcTemplate readOnlyJdbcTemplate;
     private final ModelMapper mapper;
 
-    public void fetchReportResults(String sqlCommand, List<MappingTable> mappingTableObjectList)
+    private void fetchReportResults(List<MappingTable> mappingTableObjectList,
+                                   String sqlCommand,
+                                   Object ... args)
             throws DatabaseReadException, ReportIdNotFoundException {
         List<Map<String, Object>> resultList = null;
 
         try {
             log.info("Reading mapping data");
-            resultList = readOnlyJdbcTemplate.queryForList(sqlCommand);
+            if (args != null && args.length > 0) {
+                resultList = readOnlyJdbcTemplate.queryForList(sqlCommand, args);
+            }
+            else {
+                resultList = readOnlyJdbcTemplate.queryForList(sqlCommand);
+            }
         } catch (DataAccessException e) {
             throw new DatabaseReadException("Error reading from DB: " + e);
         }
@@ -52,15 +59,14 @@ public class MappingTableDao {
 
     public MappingTable fetchReport(int requestId) throws DatabaseReadException {
         List<MappingTable> mappingTableObjectList = new ArrayList<>();
-        String sqlCommand = SELECT_SINGLE_ITEM_SQL + requestId;
-        fetchReportResults(sqlCommand, mappingTableObjectList);
+        fetchReportResults(mappingTableObjectList, SELECT_SINGLE_ITEM_SQL, requestId);
         return mappingTableObjectList.get(0);
     }
 
     public List<MappingTable> fetchReportList() throws DatabaseReadException {
         List<MappingTable> mappingTableObjectList = new ArrayList<>();
 
-        fetchReportResults(SELECT_SQL, mappingTableObjectList);
+        fetchReportResults(mappingTableObjectList, SELECT_SQL);
 
         return mappingTableObjectList;
     }
