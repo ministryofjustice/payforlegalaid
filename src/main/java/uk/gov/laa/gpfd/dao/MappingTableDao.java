cc17c099a16577e3f487af603eaefeaa35296abe
Jason Smallman <Jason.smallman@digital.justice.gov.uk>
2025-01-10 09:37:18 +0000

    Bug/lpf 543 wrong report being returned (#90)
    * amended report selection function to search mapping tables for id
    Signed-off-by: jason.smallman <jason.smallman@digital.justice.gov.uk>
    
    * updated in response to review
    Signed-off-by: jason.smallman <jason.smallman@digital.justice.gov.uk>
    
    * added integration test
    Signed-off-by: jason.smallman <jason.smallman@digital.justice.gov.uk>
    
    * split fetch reports methods and added common method and added tests
    Signed-off-by: jason.smallman <jason.smallman@digital.justice.gov.uk>
    
    * added tests for ReportViews dao
    Signed-off-by: jason.smallman <jason.smallman@digital.justice.gov.uk>
    
    * added parameters for queries
    Signed-off-by: jason.smallman <jason.smallman@digital.justice.gov.uk>
    
    * sonarcube corrections
    Signed-off-by: jason.smallman <jason.smallman@digital.justice.gov.uk>

--- src/main/java/uk/gov/laa/gpfd/dao/MappingTableDao.java
+++ src/main/java/uk/gov/laa/gpfd/dao/MappingTableDao.java
@@ -19,18 +19,26 @@ import java.util.Map;
 @Repository
 @RequiredArgsConstructor
 public class MappingTableDao {
-    private static final String SELECT_SQL = "SELECT * FROM GPFD.CSV_TO_SQL_MAPPING_TABLE";
+    private static final String SELECT_SQL = "SELECT * FROM GPFD.CSV_TO_SQL_MAPPING_TABLE ORDER BY ID ASC";
+    private static final String SELECT_SINGLE_ITEM_SQL = "SELECT * FROM GPFD.CSV_TO_SQL_MAPPING_TABLE WHERE ID = ?";
 
     private final JdbcTemplate readOnlyJdbcTemplate;
     private final ModelMapper mapper;
 
-    public List<MappingTable> fetchReportList() throws DatabaseReadException {
-        List<MappingTable> mappingTableObjectList = new ArrayList<>();
-        List<Map<String, Object>> resultList;
+    private void fetchReportResults(List<MappingTable> mappingTableObjectList,
+                                   String sqlCommand,
+                                   Object ... args)
+            throws DatabaseReadException, ReportIdNotFoundException {
+        List<Map<String, Object>> resultList = null;
 
         try {
             log.info("Reading mapping data");
-            resultList = readOnlyJdbcTemplate.queryForList(SELECT_SQL);
+            if (args != null && args.length > 0) {
+                resultList = readOnlyJdbcTemplate.queryForList(sqlCommand, args);
+            }
+            else {
+                resultList = readOnlyJdbcTemplate.queryForList(sqlCommand);
+            }
         } catch (DataAccessException e) {
             throw new DatabaseReadException("Error reading from DB: " + e);
         }
@@ -47,6 +55,18 @@ public class MappingTableDao {
         } catch (MappingException e) {
             log.error("Exception with model map loop: %s", e);
         }
+    }
+
+    public MappingTable fetchReport(int requestId) throws DatabaseReadException {
+        List<MappingTable> mappingTableObjectList = new ArrayList<>();
+        fetchReportResults(mappingTableObjectList, SELECT_SINGLE_ITEM_SQL, requestId);
+        return mappingTableObjectList.get(0);
+    }
+
+    public List<MappingTable> fetchReportList() throws DatabaseReadException {
+        List<MappingTable> mappingTableObjectList = new ArrayList<>();
+
+        fetchReportResults(mappingTableObjectList, SELECT_SQL);
 
         return mappingTableObjectList;
     }
