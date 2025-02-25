ff42b5dca7f76f4f4e21bbce9e6fb42fb654dec4
jason.smallman <jason.smallman@digital.justice.gov.uk>
2025-02-25 13:35:13 +0000

    added dao classes Signed-off-by: jason.smallman <jason.smallman@digital.justice.gov.uk>

---
+++ src/test/resources/gpfd_reports_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO GPFD.REPORT_OUTPUT_TYPES(ID,EXTENSION,DESCRIPTION)
+VALUES
+    ('6ebd27ac-4d83-485d-a4fd-3e45f9a53484', 'csv', 'Comma Separated Text'),
+    ('bd098666-94e4-4b0e-822c-8e5dfb04c908','xlsx','Excel Document')
+;
+
