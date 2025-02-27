15bddff21ccde37f002acb9a9905659e9ce3b209
robert.buczek <robert.buczek@digital.justice.gov.uk>
2025-02-27 18:37:39 +0000

    [LPF-626]: Create CCMS Invoice Analysis report
    Signed-off-by: robert.buczek <robert.buczek@digital.justice.gov.uk>

--- src/main/java/uk/gov/laa/gpfd/controller/ReportsController.java
+++ src/main/java/uk/gov/laa/gpfd/controller/ReportsController.java
@@ -1,6 +1,5 @@
 package uk.gov.laa.gpfd.controller;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -10,8 +9,9 @@ import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2Aut
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+import uk.gov.laa.gpfd.api.ExcelApi;
 import uk.gov.laa.gpfd.api.ReportsApi;
 import uk.gov.laa.gpfd.model.GetReportById200Response;
 import uk.gov.laa.gpfd.model.ReportsGet200Response;
@@ -21,17 +21,23 @@ import uk.gov.laa.gpfd.services.ReportTrackingTableService;
 import uk.gov.laa.gpfd.services.StreamingService;
 import uk.gov.laa.gpfd.services.ReportManagementService;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-public class ReportsController implements ReportsApi {
+public class ReportsController implements ReportsApi, ExcelApi {
 
     private final ReportTrackingTableService reportTrackingTableService;
     private final ReportService reportService;
     private final ReportManagementService reportManagementService;
     private final StreamingService streamingService;
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.empty();
+    }
 
     @Override
     public ResponseEntity<GetReportById200Response> getReportById(UUID id) {
@@ -69,27 +75,32 @@ public class ReportsController implements ReportsApi {
     }
 
     /**
-     * Handles HTTP GET requests to stream an Excel file to the client. This endpoint is designed to
-     * asynchronously generate and stream an Excel file based on the provided unique identifier.
+     * Handles HTTP GET requests to stream an Excel file to the client. This endpoint generates an Excel file
+     * based on the provided unique identifier (UUID) and streams it directly to the client as a downloadable attachment.
      *
-     * <p>The response is streamed directly to the client with the content type set to
-     * {@link MediaType#APPLICATION_OCTET_STREAM_VALUE}, ensuring the file is downloaded as an attachment.
-     * The method uses a {@link DeferredResult} to handle the asynchronous nature of the operation,
-     * allowing the server to process other requests while the Excel file is being generated and streamed.
+     * <p>The response is streamed with the content type set to
+     * {@code application/vnd.openxmlformats-officedocument.spreadsheetml.sheet}, which is the standard MIME type
+     * for Excel files (.xlsx). This ensures compatibility with Excel and other spreadsheet software.
      *
      * <p>Example usage:
      * <pre>
      * GET /excel/b36f9bbb-1178-432c-8f99-8090e285f2d3
      * </pre>
      *
-     * @param response    the {@link HttpServletResponse} to which the Excel file will be streamed
-     * @param requestedId the unique identifier (UUID) of the report to be generated and streamed
-     * @return a {@link DeferredResult} representing the asynchronous result of the streaming operation
+     * <p>If the Excel file is successfully generated and streamed, the response will include:
+     * <ul>
+     *   <li>A {@code Content-Disposition} header with the filename in the format {@code <report_name>.xlsx}.</li>
+     *   <li>A {@code Content-Type} header set to {@code application/vnd.openxmlformats-officedocument.spreadsheetml.sheet}.</li>
+     * </ul>
+     *
+     * <p>If an error occurs during the process, the method will log the error and throw an appropriate exception.
+     *
+     * @param id The unique identifier (UUID) of the report to be generated and streamed as an Excel file.
+     * @return A {@link ResponseEntity} containing a {@link StreamingResponseBody} for the Excel file.
+     * @throws TransferException.StreamException.ExcelStreamWriteException If an error occurs while streaming the Excel data.
      */
-    @SuppressWarnings("java:S6856") // Expose later endpoint
-    public DeferredResult<StreamingResponseBody> getExcel(HttpServletResponse response, @PathVariable(value = "id") UUID requestedId) {
-        log.info("Request received for streaming Excel file with ID: {}", requestedId);
-        return streamingService.streamExcel(response, requestedId);
+    @Override
+    public ResponseEntity<StreamingResponseBody> getExcelById(UUID id) {
+        return streamingService.streamExcel(id);
     }
-
 }
