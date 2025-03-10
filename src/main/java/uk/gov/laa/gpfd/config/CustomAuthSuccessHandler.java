b65de45056cfe9f7fed0ea0d6ea47c932e6fb630
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-03-10 17:09:21 +0000

    LPF-664 - consistent naming

--- src/main/java/uk/gov/laa/gpfd/config/CustomAuthSuccessHandler.java
+++ src/main/java/uk/gov/laa/gpfd/config/CustomAuthSuccessHandler.java
@@ -25,12 +25,12 @@ public class CustomAuthSuccessHandler implements AuthenticationSuccessHandler {
         log.debug("Authentication successful");
 
         HttpSession session = request.getSession();
-        Object redirectUrlObject = session.getAttribute("redirect_uri");
+        Object redirectUriObject = session.getAttribute("redirect_uri");
         Object savedRequestObject = session.getAttribute("SPRING_SECURITY_SAVED_REQUEST");
 
         boolean validRedirectUri = false;
 
-        if (redirectUrlObject instanceof String redirectUri && !redirectUri.isEmpty()) {
+        if (redirectUriObject instanceof String redirectUri && !redirectUri.isEmpty()) {
             if (appConfig.isValidRedirectUri(redirectUri)) {
                 validRedirectUri = true;
             } else {
@@ -40,16 +40,16 @@ public class CustomAuthSuccessHandler implements AuthenticationSuccessHandler {
         }
 
         if (validRedirectUri) {
-            // We passed a redirect url into our auth request (aka request is from the ui) and it is whitelisted
-            log.info("After authentication we are returning to the request redirect url");
-            response.sendRedirect(redirectUrlObject.toString());
+            // We passed a redirect uri into our auth request (aka request is from the ui) and it is whitelisted
+            log.info("After authentication we are returning to the request's Redirect URI");
+            response.sendRedirect(redirectUriObject.toString());
         } else if (savedRequestObject instanceof SavedRequest savedRequest) {
             // We came here from another part of the api, such as trying to load the /reports endpoint without being logged in
-            log.info("After authentication we are returning to their original url");
+            log.info("After authentication we are returning to their original URI");
             response.sendRedirect(savedRequest.getRedirectUrl());
         } else {
             // Else just let Spring do what it wants to do - will probably be the redirect-uri-template
-            log.info("After authentication, redirect to the default url");
+            log.info("After authentication, redirect to the default URI");
         }
 
     }
