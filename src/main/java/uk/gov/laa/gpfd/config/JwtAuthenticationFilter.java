40451823a5debcb41b82a8fa0f8f4f9c86f7ac7f
sinead.cummings <sinead.cummings@justice.gov.uk>
2025-03-27 15:51:19 +0000

    lpf-735: remove unused imports

--- src/main/java/uk/gov/laa/gpfd/config/JwtAuthenticationFilter.java
+++ src/main/java/uk/gov/laa/gpfd/config/JwtAuthenticationFilter.java
@@ -2,8 +2,6 @@ package uk.gov.laa.gpfd.config;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
