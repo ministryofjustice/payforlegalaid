54c4cdcf6f0dc3223ac7d8a9b97af70e92ab090d
SineadCummings <sinead.cummings@justice.gov.uk>
2025-03-28 12:36:21 +0000

    lpf-735: add logic to use default auth behaviour when no auth token (#182)
    * lpf-735: add logic to use default security filter when no auth token provided
    
    * lpf-735: Update types to remove unnecessary casting, remove unused imports
    
    * lpf-735: remove unused imports
    
    * lpf-735: doFilter must always be called to ensure rest of auth flow continues
    
    * lpf-735: add missing test
    
    * lpf-735: use parameterised test
    
    * lpf-735: remove unused import

---
+++ src/main/java/uk/gov/laa/gpfd/config/JwtAuthenticationFilter.java
@@ -0,0 +1,24 @@
+package uk.gov.laa.gpfd.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
+public class JwtAuthenticationFilter {
+
+    public void doFilter(HttpServletRequest servletRequest, HttpServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        var token = servletRequest.getHeader("Authorization");
+
+        if (token != null && !token.isEmpty()) {
+            // JWT validation logic to come in later PR
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+
+    }
+}
---
+++ src/test/java/uk/gov/laa/gpfd/config/JwtAuthenticationFilterTest.java
@@ -0,0 +1,59 @@
+package uk.gov.laa.gpfd.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class JwtAuthenticationFilterTest {
+
+    @Mock
+    private AppConfig appConfig;
+
+    @Mock
+    private JwtDecoder jwtDecoder;
+
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Mock
+    private HttpServletRequest mockRequest;
+
+    @Mock
+    private HttpServletResponse mockResponse;
+
+    @Mock
+    private FilterChain mockFilterChain;
+
+    @Mock
+    private SecurityContext securityContext;
+
+    @BeforeEach
+    void beforeEach() {
+        reset(mockRequest, mockRequest, mockFilterChain, jwtDecoder, appConfig);
+        SecurityContextHolder.setContext(securityContext);
+        jwtAuthenticationFilter = new JwtAuthenticationFilter();
+    }
+
+    @ParameterizedTest
+    @SneakyThrows
+    @NullAndEmptySource
+    @ValueSource(strings = "Bearer a.valid.token")
+    void shouldUseDefaultJourneyIfNullToken(String token) {
+        when(mockRequest.getHeader("Authorization")).thenReturn(token);
+        jwtAuthenticationFilter.doFilter(mockRequest, mockResponse, mockFilterChain);
+        verify(mockFilterChain).doFilter(mockRequest, mockResponse);
+    }
+}
