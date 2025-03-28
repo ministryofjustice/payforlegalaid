2fee552de3808fee43604ac74bea500dbb454066
sinead.cummings <sinead.cummings@justice.gov.uk>
2025-03-28 15:07:15 +0000

    removed unused import

--- src/main/java/uk/gov/laa/gpfd/config/JwtAuthenticationFilter.java
+++ src/main/java/uk/gov/laa/gpfd/config/JwtAuthenticationFilter.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.security.oauth2.jwt.JwtException;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
