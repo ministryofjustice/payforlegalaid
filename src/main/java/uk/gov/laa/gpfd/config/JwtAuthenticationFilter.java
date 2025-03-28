92a07f29a52527a84d509016d8f3ba1656b769ea
SineadCummings <sinead.cummings@justice.gov.uk>
2025-03-28 16:03:10 +0000

    lpf-736: extract and validate JWT format (#184)
    * lpf-735: add logic to use default security filter when no auth token provided
    
    * lpf-736: add code to validate token is in expected format
    
    * lpf-735: Update types to remove unnecessary casting, remove unused imports
    
    * lpf-735: remove unused imports
    
    * lpf-736: parameterised tests
    
    * lpf-735: doFilter must always be called to ensure rest of auth flow continues
    
    * lpf-735: add missing test
    
    * lpf-735: use parameterised test
    
    * lpf-735: remove unused import
    
    * lpf-736: add extends filter to ensure this logic is called only once and also implement doFilterInternal for same reason
    
    * not to be added in this class, to be added later with @Bean
    
    * updated string var to constant
    
    * removed unused import
    
    * add extra test to confirm token with extra parts also not valid
    
    * add tests for null and empty
    
    * remove unused import

--- src/main/java/uk/gov/laa/gpfd/config/JwtAuthenticationFilter.java
+++ src/main/java/uk/gov/laa/gpfd/config/JwtAuthenticationFilter.java
@@ -5,13 +5,20 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Slf4j
-public class JwtAuthenticationFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    public void doFilter(HttpServletRequest servletRequest, HttpServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    private static final String TOKEN_PREFIX = "bearer ";
+    private static final int TOKEN_PARTS = 3;
+
+    @Override
+    public void doFilterInternal(HttpServletRequest servletRequest, @NotNull HttpServletResponse servletResponse, @NotNull FilterChain filterChain) throws IOException, ServletException {
         var token = servletRequest.getHeader("Authorization");
 
         if (token != null && !token.isEmpty()) {
@@ -21,4 +28,31 @@ public class JwtAuthenticationFilter {
         filterChain.doFilter(servletRequest, servletResponse);
 
     }
+
+    public String extractJwtToken(String token) {
+        final String INVALID_JWT_ERROR_MESSAGE = "Token is not a valid JWT";
+
+        if (token == null || token.length() <= TOKEN_PREFIX.length())
+            throw new JwtException(INVALID_JWT_ERROR_MESSAGE);
+
+        if (!token.substring(0, TOKEN_PREFIX.length()).equalsIgnoreCase(TOKEN_PREFIX))
+            throw new JwtException(INVALID_JWT_ERROR_MESSAGE);
+
+        token = token.substring(TOKEN_PREFIX.length());
+
+        var contents = token.split("\\.");
+
+        if (contents.length != TOKEN_PARTS) {
+            throw new JwtException(INVALID_JWT_ERROR_MESSAGE);
+        }
+
+        for (String s : contents) {
+            if (s.isEmpty()) {
+                throw new JwtException(INVALID_JWT_ERROR_MESSAGE);
+            }
+        }
+
+        return token;
+    }
+
 }
--- src/test/java/uk/gov/laa/gpfd/config/JwtAuthenticationFilterTest.java
+++ src/test/java/uk/gov/laa/gpfd/config/JwtAuthenticationFilterTest.java
@@ -7,15 +7,20 @@ import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
 
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class JwtAuthenticationFilterTest {
@@ -55,5 +60,19 @@ class JwtAuthenticationFilterTest {
         when(mockRequest.getHeader("Authorization")).thenReturn(token);
         jwtAuthenticationFilter.doFilter(mockRequest, mockResponse, mockFilterChain);
         verify(mockFilterChain).doFilter(mockRequest, mockResponse);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Bearer ", "bearer ", "BEARER "})
+    void shouldReturnJwtForValidBearerToken(String tokenPrefix) {
+        assertEquals("aaaa.bbbb.cccc", jwtAuthenticationFilter.extractJwtToken(tokenPrefix + "aaaa.bbbb.cccc"));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"ThisIsNotValid", "aaaa.bbbb.cccc", "Bearer ThisIsNotValidEither", "Bearer aaaa.", "Bearer aaaaa.bbbbbbb.cccc.ddddddd", "Bearer .bbbb.cccc", "Bearer aaaaa..cccc", "Bearer aaaaa.bbbb.", "Bea", "Bearer "})
+    void shouldThrowIfTokenNotRightFormat(String token) {
+        Exception ex = assertThrows(JwtException.class, () -> jwtAuthenticationFilter.extractJwtToken(token));
+        assertEquals("Token is not a valid JWT", ex.getMessage());
     }
 }
