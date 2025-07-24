cb74aa0610089741436f6cd60244e90ea5e9337f
Robert Buczek <142578199+RobertBuczek@users.noreply.github.com>
2025-07-24 13:37:04 +0100

    [LPF-966]: Add integration tests for performance improvement code (#275)

--- src/it/java/uk/gov/laa/gpfd/integration/GetReportsIT.java
+++ src/it/java/uk/gov/laa/gpfd/integration/GetReportsIT.java
@@ -1,8 +1,13 @@
 package uk.gov.laa.gpfd.integration;
 
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,42 +17,44 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import uk.gov.laa.gpfd.config.TestDatabaseConfig;
+import uk.gov.laa.gpfd.integration.verifier.DatabaseVerifier;
+import uk.gov.laa.gpfd.integration.verifier.DatabaseVerifier.Table;
 
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        classes = {
-                TestDatabaseConfig.class
-        }
-)
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = {TestDatabaseConfig.class})
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestInstance(PER_CLASS)
 @TestPropertySource(locations = "classpath:application-test.yml")
-class GetReportsIT extends BaseIT {
-
-    public static final int NUMBER_OF_REPORTS_IN_TEST_DATA = 10;
+final class GetReportsIT extends BaseIT {
 
     @Autowired
-    private JdbcTemplate writeJdbcTemplate;
+    private JdbcTemplate jdbc;
 
     @Test
-    void shouldReturnListOfReports() throws Exception {
+    @SneakyThrows
+    void shouldSuccessfullyReturnAllAvailableReports() {
+        var reportsLen = DatabaseVerifier.rowCountFor(Table.REPORTS).apply(jdbc);
+
         performGetRequest("/reports")
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.reportList").isArray())
-            .andExpect(jsonPath("$.reportList.length()").value(NUMBER_OF_REPORTS_IN_TEST_DATA));
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.reportList").isArray())
+                .andExpect(jsonPath("$.reportList.length()").value(reportsLen));
     }
 
     @Test
-    void shouldReturn200WhenNoReportsFound() throws Exception {
-        writeJdbcTemplate.update("ALTER TABLE GPFD.REPORTS DROP CONSTRAINT fk_report_output_types_report_id");
-        writeJdbcTemplate.update("ALTER TABLE GPFD.REPORT_GROUPS DROP CONSTRAINT fk_report_groups_report_id");
-        writeJdbcTemplate.update("ALTER TABLE GPFD.REPORT_QUERIES DROP CONSTRAINT fk_report_queries_report_id");
-        writeJdbcTemplate.update("ALTER TABLE GPFD.FIELD_ATTRIBUTES DROP CONSTRAINT fk_field_attributes_report_query_id");
-        writeJdbcTemplate.update("ALTER TABLE GPFD.REPORTS_TRACKING DROP CONSTRAINT fk_reports_tracking_reports_id");
-        writeJdbcTemplate.update("TRUNCATE TABLE GPFD.REPORTS");
+    @SneakyThrows
+    void shouldSuccessfullyReturn200WhenNoReportsFound() {
+        jdbc.update("ALTER TABLE GPFD.REPORTS DROP CONSTRAINT fk_report_output_types_report_id");
+        jdbc.update("ALTER TABLE GPFD.REPORT_GROUPS DROP CONSTRAINT fk_report_groups_report_id");
+        jdbc.update("ALTER TABLE GPFD.REPORT_QUERIES DROP CONSTRAINT fk_report_queries_report_id");
+        jdbc.update("ALTER TABLE GPFD.FIELD_ATTRIBUTES DROP CONSTRAINT fk_field_attributes_report_query_id");
+        jdbc.update("ALTER TABLE GPFD.REPORTS_TRACKING DROP CONSTRAINT fk_reports_tracking_reports_id");
+        jdbc.update("TRUNCATE TABLE GPFD.REPORTS");
 
         performGetRequest("/reports")
-            .andExpect(status().is2xxSuccessful());
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.reportList").isEmpty());
     }
 }
