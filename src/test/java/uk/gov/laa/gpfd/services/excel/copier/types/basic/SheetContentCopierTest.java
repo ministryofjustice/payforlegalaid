1d511737e8aa6bdf4759cfc28e17587a164b96c5
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-08-11 14:59:00 +0100

    LPF-1009 - remove unnecessary public

--- src/test/java/uk/gov/laa/gpfd/services/excel/copier/types/basic/SheetContentCopierTest.java
+++ src/test/java/uk/gov/laa/gpfd/services/excel/copier/types/basic/SheetContentCopierTest.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class SheetContentCopierTest {
+class SheetContentCopierTest {
 
     private static final BiConsumer<Sheet, Sheet> copyAll = SheetContentCopier::copyAll;
 
