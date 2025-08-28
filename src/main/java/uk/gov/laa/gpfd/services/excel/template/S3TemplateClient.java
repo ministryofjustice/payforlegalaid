e9f23812b42f257fb7280ebf36bef2b6c5425b78
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-08-28 11:45:09 +0100

    LPF-1023 - test out s3 client

--- src/main/java/uk/gov/laa/gpfd/services/excel/template/S3TemplateClient.java
+++ src/main/java/uk/gov/laa/gpfd/services/excel/template/S3TemplateClient.java
@@ -1,5 +1,8 @@
 package uk.gov.laa.gpfd.services.excel.template;
 
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import uk.gov.laa.gpfd.exception.TemplateResourceException;
 
 import java.io.InputStream;
@@ -10,11 +13,26 @@ public record S3TemplateClient() implements TemplateClient {
     @Override
     public InputStream findTemplateById(UUID id) {
 
+        System.out.println("CCCCC - Using S3 One");
+
         var filename = getFileNameFromId(id);
 
         if (filename == null) {
             return null;
         }
+
+        S3Client s3 = S3Client.builder()
+                .region(Region.of("eu-west-2"))
+                .build();
+
+        var getObjectRequest = GetObjectRequest.builder()
+                .bucket("laa-get-payments-finance-data-dev-file-store")
+                .key(filename)
+                .build();
+
+        var file = s3.getObject(getObjectRequest);
+
+        System.out.println(file);
 
         // todo - use s3 client
 
