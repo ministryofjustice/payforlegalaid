6ac3443d4a3d329c0089ef659e686907f9986f14
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-08-28 11:08:32 +0100

    LPF-1023 - Update local template client to use new shared routine

--- src/main/java/uk/gov/laa/gpfd/services/excel/template/LocalTemplateClient.java
+++ src/main/java/uk/gov/laa/gpfd/services/excel/template/LocalTemplateClient.java
@@ -21,22 +21,9 @@ public record LocalTemplateClient() implements TemplateClient {
     @Override
     @SneakyThrows
     public InputStream findTemplateById(UUID id) {
-        if (id == null) {
-            throw new IllegalArgumentException("Template ID cannot be null or blank");
-        }
 
-        var filename = switch(id.toString()) {
-            case "eee30b23-2c8d-4b4b-bb11-8cd67d07915c" -> "CCMS_AND_CIS_BANK_ACCOUNT_REPORT_W_CATEGORY_CODE_YTD.xlsx";
-            case "a017241a-359f-4fdb-a0cd-7f28f1946ef1" -> "CCMS_AND_CIS_BANK_ACCOUNT_REPORT_W_CATEGORY_CODE_MNTH.xlsx";
-            case "f46b4d3d-c100-429a-bf9a-223305dbdbfb" -> null;
-            case "00000000-0000-0000-0000-000000000000" -> "CCMS_invoice analysis_template_v1_1.xlsx";
-            case "7073dd13-e325-4863-a05c-a049a815d1f7" -> "LEGAL_HELP_CONTRACT_BALANCES.xlsx";
-            case "7bda9aa4-6129-4c71-bd12-7d4e46fdd882" -> "LATE_PROCESSED_BILLS.xlsx";
-            case "b380e788-2096-46dc-b58a-21bf771669dc" -> "MEDIATION_CONTRACT_BALANCES_TEMPLATE.xlsx";
-            case "8b9f0484-819f-4e0f-b60a-0b3f9d30d9ba" -> "CCMS_THIRD_PARTY_REPORT.xlsx";
-            case "22fe2b17-eea8-4c74-929d-9c69503f25d3" -> "C12_LATE_PROCESSED_BILLS_CIS_TEMPLATE.xlsx";
-            default -> throw new TemplateNotFoundException("Template not found in resources for ID: " + id);
-        };
+
+        var filename = getFileNameFromId(id);
 
         if (filename == null) {
             return null;
