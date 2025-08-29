e61636aaa137780299306a4d331ae6dc59d25dfa
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-08-29 12:07:22 +0100

    LPF-1023 - switch from profile driven to property driven behaviour

--- src/main/java/uk/gov/laa/gpfd/config/S3Config.java
+++ src/main/java/uk/gov/laa/gpfd/config/S3Config.java
@@ -3,11 +3,13 @@ package uk.gov.laa.gpfd.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import uk.gov.laa.gpfd.services.excel.template.S3TemplateClient;
 import uk.gov.laa.gpfd.services.excel.template.TemplateClient;
 
+@Configuration
 @ConditionalOnProperty(name = "gpfd.s3.use-template-store", havingValue = "true")
 public class S3Config {
 
