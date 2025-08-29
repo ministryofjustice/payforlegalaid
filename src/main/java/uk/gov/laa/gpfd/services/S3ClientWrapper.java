285e0190c7bb8fa399bf6266ea9062ba30078027
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-08-29 16:27:30 +0100

    LPF-1023 - ensure stream gets closed to free up connections

--- src/main/java/uk/gov/laa/gpfd/services/S3ClientWrapper.java
+++ src/main/java/uk/gov/laa/gpfd/services/S3ClientWrapper.java
@@ -1,10 +1,12 @@
 package uk.gov.laa.gpfd.services;
 
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
 import software.amazon.awssdk.core.retry.RetryMode;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 
 import java.io.InputStream;
 import java.time.Duration;
@@ -16,7 +18,7 @@ public class S3ClientWrapper {
 
     public S3ClientWrapper(String awsRegion, String s3Bucket) {
 
-        // By default, AWS does not limit API calls. Set some to avoid any risk of calls hanging
+        // By default, AWS does not time out API calls. Set some to avoid any risk of calls hanging
         var config = ClientOverrideConfiguration.builder()
                 .apiCallAttemptTimeout(Duration.ofSeconds(3))
                 .apiCallTimeout(Duration.ofSeconds(10))
@@ -29,7 +31,7 @@ public class S3ClientWrapper {
         this.s3Bucket = s3Bucket;
     }
 
-    public InputStream getTemplate(String filename){
+    public ResponseInputStream<GetObjectResponse> getTemplate(String filename){
 
         var getObjectRequest = GetObjectRequest.builder()
                 .bucket(s3Bucket)
