192008b6b76c48806777be7cfda225210c6dfc8e
neeta.shinde <neeta.shinde@justice.gov.uk>
2025-10-28 15:32:14 +0000

    added logs for /reports endpoint.

--- src/main/java/uk/gov/laa/gpfd/controller/ui/ReportsViewController.java
+++ src/main/java/uk/gov/laa/gpfd/controller/ui/ReportsViewController.java
@@ -2,13 +2,17 @@ package uk.gov.laa.gpfd.controller.ui;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import uk.gov.laa.gpfd.api.ReportsApi;
 
 import java.net.URI;
+import java.net.http.HttpResponse;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -34,16 +38,24 @@ public class ReportsViewController {
                 URI reportDownloadUrl
         ) {
         }
-        var reportList = Objects.requireNonNull(api.reportsGet().getBody()).getReportList().stream()
-                .map(reportItem ->
-                        new Dto(
-                                reportItem.getId(),
-                                reportItem.getReportName(),
-                                reportItem.getDescription(),
-                                api.getReportById(reportItem.getId()).getBody().getReportDownloadUrl()
-                        )
-                )
-                .toList();
+        log.info("Request to get all reports");
+        List<Dto> reportList = null;
+        try {
+            reportList = Objects.requireNonNull(api.reportsGet().getBody()).getReportList().stream()
+                    .map(reportItem ->
+                            new Dto(
+                                    reportItem.getId(),
+                                    reportItem.getReportName(),
+                                    reportItem.getDescription(),
+                                    api.getReportById(reportItem.getId()).getBody().getReportDownloadUrl()
+                            )
+                    )
+                    .toList();
+            log.info("Responding with status: {} Total reports found: {}", HttpStatus.OK.value(), reportList.size());
+        } catch (Exception e) {
+            log.error("Failed to fetch reports: {}", e.getMessage());
+            model.addAttribute("reportListResponse", List.of());
+        }
 
         model.addAttribute("reportListResponse", reportList);
         return "reports/list";
