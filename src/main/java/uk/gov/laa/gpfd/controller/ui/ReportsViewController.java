074a836284b0fdef4e77cae9f9088b01556e536a
neeta.shinde <neeta.shinde@justice.gov.uk>
2025-10-30 10:35:56 +0000

    refactor code

--- src/main/java/uk/gov/laa/gpfd/controller/ui/ReportsViewController.java
+++ src/main/java/uk/gov/laa/gpfd/controller/ui/ReportsViewController.java
@@ -2,16 +2,13 @@ package uk.gov.laa.gpfd.controller.ui;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.Response;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import uk.gov.laa.gpfd.api.ReportsApi;
 
 import java.net.URI;
-import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
