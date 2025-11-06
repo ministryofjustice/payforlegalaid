53f18da974e2aaa6b05899e49a8e74bba6d5f450
Chris Cook <114592674+ChrisCookOC@users.noreply.github.com>
2025-11-06 13:17:33 +0000

    Tweak logs to make them easier to play with in OpenSearch (#337)

--- src/main/java/uk/gov/laa/gpfd/config/RequestResponseInterceptor.java
+++ src/main/java/uk/gov/laa/gpfd/config/RequestResponseInterceptor.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Slf4j
@@ -12,13 +13,13 @@ public class RequestResponseInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        log.info("Request received: {} {}", request.getMethod(), request.getRequestURI());
+        log.info("Request received: {} {} ({})", request.getMethod(), request.getRequestURI(), ((HandlerMethod) handler).getMethod().getName());
         return true;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-        log.info("Completed request: {} with status {}", request.getRequestURI(), response.getStatus());
+        log.info("Completed request: {} with status {} ({})", request.getRequestURI(), response.getStatus(), ((HandlerMethod) handler).getMethod().getName());
         if (ex != null) {
             log.error("Exception occurred: {}", ex.getMessage());
         }
