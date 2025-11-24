399c88f78ba81c5ce718ab05d534bb51a5ef4489
max mills <8balldigitalsolutions@gmail.com>
2025-11-24 13:04:24 +0000

    rm whitespace

--- src/main/java/uk/gov/laa/gpfd/config/TimeBasedAccessInterceptor.java
+++ src/main/java/uk/gov/laa/gpfd/config/TimeBasedAccessInterceptor.java
@@ -25,7 +25,6 @@ public class TimeBasedAccessInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServiceUnavailableException {
         LocalTime now = timeProvider.getCurrentTime();
-        
         if (now.isBefore(startTime) || now.isAfter(endTime)) {
             throw new ServiceUnavailableException("The service is unavailable between the hours of 22:00 and 07:00, Mon - Sun");
         }
