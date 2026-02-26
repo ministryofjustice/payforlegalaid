6af3d0ab6484a94785e785c8a6c7b8808204b2f7
neeta.shinde <neeta.shinde@justice.gov.uk>
2026-02-26 12:59:03 +0000

    refactored securityConfig
    refactored extractRoles code.
    
    Fixed review comments
    
    deleted unused code
    
    fixed review comments
    
    Fixed test files review comments.
    
    deleted unwanted mocking

--- src/test/java/uk/gov/laa/gpfd/dao/ReportDaoTest.java
+++ src/test/java/uk/gov/laa/gpfd/dao/ReportDaoTest.java
@@ -10,7 +10,6 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.security.access.AccessDeniedException;
 import uk.gov.laa.gpfd.dao.support.ReportWithQueriesAndFieldAttributesExtractor;
 import uk.gov.laa.gpfd.data.ReportsTestDataFactory;
 import uk.gov.laa.gpfd.exception.ReportAccessException;
