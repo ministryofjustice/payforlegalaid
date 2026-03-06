bb18cbab6eef8e61975e60506962e75008a7c031
Chris Cook <christopher.cook@digital.justice.gov.uk>
2026-03-06 17:55:24 +0000

    LPF-1278 - debug

--- src/main/java/uk/gov/laa/gpfd/dao/sql/ResultSetExtractorHelper.java
+++ src/main/java/uk/gov/laa/gpfd/dao/sql/ResultSetExtractorHelper.java
@@ -4,11 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-@Slf4j
+/**
+ * Wrapper to use our prexisting {@link RowCallbackHandler}s with a {@link ResultSetExtractor}
+ * This is because upgrading to Spring Boot 4, the {@link JdbcTemplate} was not playing nicely with our RowCallbackHandlers
+ * But there is lots of logic in them that we don't really want to rewrite for a new paradigm right now
+ * @param <T>
+ */
 public class ResultSetExtractorHelper<T> implements ResultSetExtractor<T> {
 
     private final RowCallbackHandler rowCallbackHandler;
@@ -19,13 +25,8 @@ public class ResultSetExtractorHelper<T> implements ResultSetExtractor<T> {
 
     @Override
     public T extractData(ResultSet rs) throws SQLException, DataAccessException {
-        long last = System.nanoTime();
-        while (rs.next()){
-            long now = System.nanoTime();
-            long deltaMicros = (now - last) / 1000;
-            log.warn("Calling next row been {} microseconds since last", deltaMicros);
+        while (rs.next()) {
             rowCallbackHandler.processRow(rs);
-            last = System.nanoTime();
         }
         return null;
     }
---
+++ src/test/java/uk/gov/laa/gpfd/dao/sql/ResultSetExtractorHelperTest.java
@@ -0,0 +1,89 @@
+package uk.gov.laa.gpfd.dao.sql;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.jdbc.core.RowCallbackHandler;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ResultSetExtractorHelperTest {
+
+    @Mock
+    ResultSet mockResultSet;
+    @Mock
+    RowCallbackHandler mockRowCallbackHandler;
+
+    ResultSetExtractorHelper<Void> resultSetExtractorHelper;
+
+    @BeforeEach
+    void beforeEach() {
+        resultSetExtractorHelper = new ResultSetExtractorHelper<Void>(mockRowCallbackHandler);
+        reset(mockResultSet, mockRowCallbackHandler);
+    }
+
+    @Test
+    void shouldProcessAllRows() throws Exception {
+        when(mockResultSet.next()).thenReturn(true, true, true, false);
+
+        resultSetExtractorHelper.extractData(mockResultSet);
+
+        verify(mockRowCallbackHandler, times(3)).processRow(mockResultSet);
+        verifyNoMoreInteractions(mockRowCallbackHandler);
+
+        InOrder inOrder = inOrder(mockResultSet, mockRowCallbackHandler);
+        inOrder.verify(mockResultSet).next();
+        inOrder.verify(mockRowCallbackHandler).processRow(mockResultSet);
+        inOrder.verify(mockResultSet).next();
+        inOrder.verify(mockRowCallbackHandler).processRow(mockResultSet);
+        inOrder.verify(mockResultSet).next();
+        inOrder.verify(mockRowCallbackHandler).processRow(mockResultSet);
+        inOrder.verify(mockResultSet).next();
+    }
+
+    @Test
+    void shouldDoNothingIfEmptyResultSet() throws Exception {
+        when(mockResultSet.next()).thenReturn(false);
+
+        resultSetExtractorHelper.extractData(mockResultSet);
+
+        verify(mockRowCallbackHandler, never()).processRow(any());
+        verify(mockResultSet, times(1)).next();
+    }
+
+    @Test
+    void shouldPropagateSqlExceptionFromResultSetNext() throws Exception {
+        when(mockResultSet.next()).thenThrow(new SQLException("result set error"));
+
+        assertThrows(SQLException.class, () -> resultSetExtractorHelper.extractData(mockResultSet));
+        verify(mockRowCallbackHandler, never()).processRow(any());
+    }
+
+    @Test
+    void shouldPropagateSqlExceptionFromRowCallbackHandler() throws Exception {
+        when(mockResultSet.next()).thenReturn(true);
+        doThrow(new SQLException("row callback handler error")).when(mockRowCallbackHandler).processRow(mockResultSet);
+
+        assertThrows(SQLException.class, () -> resultSetExtractorHelper.extractData(mockResultSet));
+
+        verify(mockResultSet).next();
+        verify(mockRowCallbackHandler, times(1)).processRow(mockResultSet);
+    }
+
+}
