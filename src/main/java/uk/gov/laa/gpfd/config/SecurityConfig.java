169533ea5630b4e2c4d8084f1bfe691e419943a4
neeta.shinde <neeta.shinde@justice.gov.uk>
2026-03-16 15:35:04 +0000

    added clientAuthorized manager
    updated pom.xml
    
    Added debug logs

--- src/main/java/uk/gov/laa/gpfd/config/SecurityConfig.java
+++ src/main/java/uk/gov/laa/gpfd/config/SecurityConfig.java
@@ -10,6 +10,11 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -186,4 +191,17 @@ public class SecurityConfig {
         return source;
     }
 
+
+    @Bean
+    public OAuth2AuthorizedClientRepository authorizedClientRepository() {
+        // Stores authorized clients in the HTTP session
+        return new HttpSessionOAuth2AuthorizedClientRepository();
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository) {
+        return new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientRepository);
+    }
 }
