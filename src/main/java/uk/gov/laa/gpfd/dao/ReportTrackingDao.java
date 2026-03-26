d407cdac82286526b9aebd58d3bcd6a07c6e2f75
Chris Cook <christopher.cook@digital.justice.gov.uk>
2026-03-26 14:44:22 +0000

    LPF-1127 - Add Reprot Tracking DAO

---
+++ src/main/java/uk/gov/laa/gpfd/dao/ReportTrackingDao.java
@@ -0,0 +1,33 @@
+package uk.gov.laa.gpfd.dao;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.stereotype.Service;
+import uk.gov.laa.gpfd.exception.DatabaseWriteException;
+import uk.gov.laa.gpfd.utils.SecurityUtils;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.UUID;
+
+@Slf4j
+@Service
+public record ReportTrackingDao(JdbcOperations trackingJdbcTemplate, SecurityUtils securityUtils) {
+
+    private static final String insertSql = "INSERT INTO GLAD.REPORT_TRACKING(ID, REPORT_ID, USER_ID, DOWNLOAD_TIME) VALUES (?, ?, ?, ?)";
+
+    public void insertTrackingRow(UUID reportId) {
+        log.info("Inserting report tracking row for {}", reportId);
+        var userId = UUID.fromString(securityUtils.extractUserId());
+
+        try {
+            trackingJdbcTemplate.update(insertSql, UUID.randomUUID(), reportId, userId, Timestamp.from(Instant.now()));
+        } catch (DataAccessException e) {
+            var message = "Failed to insert report tracking row for report " + reportId + " / user " + userId;
+            log.error("{} with {} exception: {}", message, e.getClass().getName(), e.getMessage());
+            throw new DatabaseWriteException(message);
+        }
+    }
+
+}
