d05b7b7c7e005c311adbdaa917ffd4d8cf5c43f1
Chris Cook <christopher.cook@digital.justice.gov.uk>
2026-03-27 16:49:01 +0000

    LPF-1127 - test updates

--- src/test/java/uk/gov/laa/gpfd/controller/ReportsControllerTest.java
+++ src/test/java/uk/gov/laa/gpfd/controller/ReportsControllerTest.java
@@ -1,7 +1,5 @@
 package uk.gov.laa.gpfd.controller;
 
-
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
