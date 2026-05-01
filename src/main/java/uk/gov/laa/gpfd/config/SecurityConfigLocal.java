ab06b214961312c61003acb9857acea13e8eb818
Chris Cook <christopher.cook@digital.justice.gov.uk>
2026-05-01 13:44:41 +0100

    LPF-1189 - missing import

--- src/main/java/uk/gov/laa/gpfd/config/SecurityConfigLocal.java
+++ src/main/java/uk/gov/laa/gpfd/config/SecurityConfigLocal.java
@@ -1,5 +1,6 @@
 package uk.gov.laa.gpfd.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
