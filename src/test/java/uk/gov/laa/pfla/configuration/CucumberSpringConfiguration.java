57b6bd9fa7c4d1bd37ef2d338ecae85bc11961a8
DevarshBarotOC <264167205+DevarshBarotOC@users.noreply.github.com>
2026-06-19 12:03:16 +0100

    LPF-1368 - Updated CucumberSpringConfiguration to use RANDOM_PORT which is more in line with acceptance testing, and imported a new file CucumberTestBeans to it for bean config.

--- src/test/java/uk/gov/laa/pfla/configuration/CucumberSpringConfiguration.java
+++ src/test/java/uk/gov/laa/pfla/configuration/CucumberSpringConfiguration.java
@@ -3,14 +3,12 @@ package uk.gov.laa.pfla.configuration;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
-import uk.gov.laa.gpfd.Application;
 
-@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @CucumberContextConfiguration
-@ActiveProfiles("testat")
-@TestPropertySource(locations = "classpath:application-testat.yml", properties = "spring.liquibase.enabled=true")
-@Import({uk.gov.laa.pfla.configuration.TestConfig.class, uk.gov.laa.pfla.scenario.ScenarioContext.class})
+@SpringBootTest(
+        classes = uk.gov.laa.pfla.Main.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@Import(CucumberTestBeans.class)
 public class CucumberSpringConfiguration {
 }
