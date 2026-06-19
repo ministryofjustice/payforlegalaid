7c3f6c3f688088d57feb9ce39699827446be2d42
DevarshBarotOC <264167205+DevarshBarotOC@users.noreply.github.com>
2026-06-19 12:15:50 +0100

    LPF-1368 - Completely refactored TestConfig and moved some beans to CucumberTestBeans.java.

--- src/test/java/uk/gov/laa/pfla/configuration/TestConfig.java
+++ src/test/java/uk/gov/laa/pfla/configuration/TestConfig.java
@@ -1,12 +1,11 @@
 package uk.gov.laa.pfla.configuration;
 
-import com.fasterxml.jackson.databind.MappingJsonFactory;
-import liquibase.integration.spring.SpringLiquibase;
-import org.apache.poi.ss.usermodel.Workbook;
+import com.fasterxml.jackson.core.TokenStreamFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -18,203 +17,96 @@ import org.springframework.security.oauth2.client.registration.InMemoryClientReg
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.web.client.RestTemplate;
-import uk.gov.laa.pfla.client.AuthenticationProvider;
-import uk.gov.laa.pfla.client.RestClient;
-import uk.gov.laa.pfla.client.RestTemplateWithErrorHandling;
-import uk.gov.laa.pfla.client.interceptor.AuthenticationInterceptor;
+import org.springframework.security.oauth2.jwt.JwtException;
 import uk.gov.laa.pfla.client.interceptor.HostInterceptor;
-import uk.gov.laa.pfla.comparator.WorkbookComparator;
-import uk.gov.laa.pfla.scenario.AuthenticationState;
-import uk.gov.laa.pfla.scenario.ScenarioContext;
-import uk.gov.laa.pfla.service.HttpProvider;
 import uk.gov.laa.pfla.util.JsonDeserializer;
-import uk.gov.laa.pfla.util.WorkbookUtil;
-import uk.gov.laa.pfla.util.workbook.WorkbookCreator;
 
 import javax.sql.DataSource;
-import java.util.function.Supplier;
 
-import static java.util.List.of;
-import static uk.gov.laa.pfla.client.AuthenticationProvider.basicAuth;
 import static uk.gov.laa.pfla.client.interceptor.HostInterceptor.withHost;
 
 @Configuration
 public class TestConfig {
 
-    @Value("${spring.liquibase.changelog}")
-    private String liquibaseChangeLog;
+    @Bean("readOnlyDataSource")
+    @ConfigurationProperties(prefix = "gpfd.datasource.read-only")
+    public DataSource readOnlyDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
 
     @Bean
-    @Qualifier("validAuthenticationInterceptor")
-    public AuthenticationInterceptor authenticationInterceptor() {
-        return this::authenticationProvider;
+    public DataSource writeDataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("org.h2.Driver");
+        ds.setUrl("jdbc:h2:file:~/localGpfdDb;MODE=Oracle");
+        ds.setUsername("sa");
+        ds.setPassword("");
+        return ds;
+    }
+
+    @Bean
+    public JdbcTemplate writeJdbcTemplate(
+            @Qualifier("writeDataSource") DataSource ds) {
+        return new JdbcTemplate(ds);
+    }
+
+    @Bean
+    @Primary
+    public JdbcTemplate jdbcTemplate(
+            @Qualifier("writeDataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return token -> {
+            if (!"valid-token".equals(token)) {
+                throw new JwtException("Invalid token");
+            }
+
+            return Jwt.withTokenValue(token)
+                    .header("alg", "none")
+                    .claim("sub", "test-user")
+                    .build();
+        };
+    }
+
+    @Bean
+    @Primary
+    public ClientRegistrationRepository clientRegistrationRepository() {
+
+        ClientRegistration registration =
+                ClientRegistration.withRegistrationId("graph")
+                        .clientId("mockClientId")
+                        .clientSecret("mockClientSecret")
+                        .scope("read")
+                        .authorizationUri("test")
+                        .redirectUri("test")
+                        .tokenUri("test")
+                        .authorizationGrantType(
+                                AuthorizationGrantType.CLIENT_CREDENTIALS)
+                        .build();
+
+        return new InMemoryClientRegistrationRepository(registration);
+    }
+
+    @Bean
+    JsonDeserializer jsonDeserializer() {
+        return new JsonDeserializer() {
+            private final ObjectMapper mapper = new ObjectMapper();
+
+            @Override
+            public TokenStreamFactory tokenStreamFactory() {
+                return mapper.getFactory();
+            }
+        };
     }
 
     @Bean
     public HostInterceptor hostInterceptor() {
         return withHost("http://localhost:8080");
     }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        return basicAuth("test-user", "test-password");
-    }
-
-    @Bean
-    @Qualifier("authenticatedRestTemplate")
-    public RestTemplateWithErrorHandling authenticatedRestTemplate(
-            @Qualifier("validAuthenticationInterceptor") AuthenticationInterceptor authenticationInterceptor,
-            HostInterceptor hostInterceptor) {
-        return new RestTemplateWithErrorHandling() {{
-            setInterceptors(of(authenticationInterceptor, hostInterceptor));
-        }};
-    }
-
-    @Bean
-    @Qualifier("invalidCredentialsRestClient")
-    public RestTemplateWithErrorHandling invalidCredentialsRestClient(HostInterceptor hostInterceptor) {
-        return new RestTemplateWithErrorHandling() {{
-            setInterceptors(of((AuthenticationInterceptor) () -> basicAuth("foo", "bar"), hostInterceptor));
-        }};
-    }
-
-    @Bean
-    public Supplier<RestTemplate> restTemplateSupplier() {
-        return RestTemplate::new;
-    }
-
-    @Bean
-    public HttpProvider httpClientServiceSupplier(RestClient restClient, ScenarioContext scenarioContext) {
-        return new HttpProvider() {
-            @Override
-            public RestTemplate getClient() {
-                return getClient(restClient, scenarioContext.getAuthenticationState());
-            }
-
-            @Override
-            public void setAuthenticationState(AuthenticationState state) {
-                scenarioContext.setAuthenticationState(state);
-            }
-        };
-    }
-
-    @Bean
-    public RestClient restClient(
-            RestTemplate restTemplate,
-            @Qualifier("authenticatedRestTemplate") RestTemplateWithErrorHandling restTemplateWithErrorHandling,
-            @Qualifier("invalidCredentialsRestClient") RestTemplateWithErrorHandling invalidCredentialsRestClient
-    ) {
-        return new RestClient() {
-            @Override
-            public RestTemplate authenticated() {
-                return restTemplateWithErrorHandling;
-            }
-
-            @Override
-            public RestTemplate invalidCredentials() {
-                return invalidCredentialsRestClient;
-            }
-
-            @Override
-            public RestTemplate unauthenticated() {
-                return restTemplate;
-            }
-        };
-    }
-
-    @Bean
-    @Primary
-    public ClientRegistrationRepository emptyClientRegistrationRepository() {
-        var localRegistration = ClientRegistration.withRegistrationId("graph")
-                .clientId("mockClientId")
-                .clientSecret("mockClientSecret")
-                .scope("read")
-                .authorizationUri("test")
-                .redirectUri("test2")
-                .tokenUri("test3")
-                .authorizationGrantType(AuthorizationGrantType.JWT_BEARER)
-                .build();
-
-        return new InMemoryClientRegistrationRepository(of(localRegistration));
-    }
-
-    @Bean
-    public WorkbookComparator workbookComparator() {
-        return new WorkbookComparator() {};
-    }
-
-    @Bean
-    public WorkbookUtil testingExcelService(ScenarioContext scenarioContext, WorkbookCreator workbookCreator) {
-        return new WorkbookUtil() {
-            @Override
-            public Workbook getExcelWorkbook() {
-                return getExcelWorkbook(scenarioContext);
-            }
-
-            @Override
-            public WorkbookCreator workbookCreator() {
-                return workbookCreator;
-            }
-        };
-    }
-
-    @Bean
-    public WorkbookCreator workbookCreator() {
-        return new WorkbookCreator() {};
-    }
-
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        return token -> {
-                // Simply parse the JWT without verification
-                return Jwt.withTokenValue(token)
-                        .build();
-        };
-    }
-
-    @Bean
-    public JsonDeserializer jsonDeserializer() {
-        return MappingJsonFactory::new;
-    }
-
-    /**
-     * Creates and configures a {@link SpringLiquibase} bean to be used for database,
-     * if the property `spring.liquibase.enabled` is set to `true` in the application properties.
-     *
-     * This method will set the data source to the specified {@link DataSource} bean, configure the
-     * change log file to be used by Liquibase, and ensure that the migrations are executed by
-     * setting {@code setShouldRun(true)}.
-     *
-     * @param dataSource The {@link DataSource} bean to be used by Liquibase for database connectivity.
-     * @return A configured {@link SpringLiquibase} instance ready for migration.
-     *
-     * @see SpringLiquibase
-     * @see DataSource
-     */
-    @Bean
-    @Primary
-    @ConditionalOnProperty(name = "spring.liquibase.enabled", havingValue = "true")
-    public SpringLiquibase liquibase(@Qualifier("writeDataSource") DataSource dataSource) {
-        SpringLiquibase liquibase = new SpringLiquibase();
-        liquibase.setDataSource(dataSource);
-        liquibase.setChangeLog(liquibaseChangeLog);
-        liquibase.setShouldRun(true);
-        return liquibase;
-    }
-
-    @Bean
-    @Primary
-    @ConfigurationProperties(prefix = "gpfd.datasource.read-only")
-    DataSource readOnlyDataSource() {
-        return new DriverManagerDataSource();
-    }
-
-    @Bean
-    @Primary
-    JdbcTemplate readOnlyJdbcTemplate(@Qualifier("readOnlyDataSource") DataSource dataSource) {
-        JdbcTemplate template = new JdbcTemplate(dataSource);
-        return template;
-    }
-
 }
