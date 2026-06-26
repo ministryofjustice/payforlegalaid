868b335140705d34faeff8d2173f1e86593d59d6
Chris Cook <christopher.cook@digital.justice.gov.uk>
2026-06-26 17:48:57 +0100

    Merge in ATs now they belong in here

--- src/it/java/uk/gov/laa/gpfd/integration/config/TestS3Config.java
+++ src/it/java/uk/gov/laa/gpfd/integration/config/TestS3Config.java
@@ -3,6 +3,7 @@ package uk.gov.laa.gpfd.integration.config;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.services.s3.S3Client;
 import uk.gov.laa.gpfd.dao.ReportDao;
 import uk.gov.laa.gpfd.services.excel.template.S3TemplateClient;
@@ -16,6 +17,7 @@ import uk.gov.laa.gpfd.services.s3.S3ClientWrapper;
 import static org.mockito.Mockito.mock;
 
 @TestConfiguration
+@Profile("!testat")
 public class TestS3Config {
 
     @Bean
