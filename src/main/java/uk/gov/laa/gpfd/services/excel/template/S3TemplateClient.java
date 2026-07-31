b116ad0f2f5f0e44fc8e434f7b2fe7e145ce82cd
Chris Cook <christopher.cook@digital.justice.gov.uk>
2026-07-31 17:08:27 +0100

    LPF-1549 - stop dependabot erroring on accessing packages we control

--- src/main/java/uk/gov/laa/gpfd/services/excel/template/S3TemplateClient.java
+++ src/main/java/uk/gov/laa/gpfd/services/excel/template/S3TemplateClient.java
@@ -1,11 +1,13 @@
 package uk.gov.laa.gpfd.services.excel.template;
 
 import lombok.SneakyThrows;
+import software.amazon.awssdk.core.sync.RequestBody;
 import uk.gov.laa.gpfd.exception.TemplateResourceException.TemplateResourceNotFoundException;
 import uk.gov.laa.gpfd.services.s3.S3ClientWrapper;
 
 import java.io.File;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 /**
@@ -30,15 +32,12 @@ public record S3TemplateClient(S3ClientWrapper s3Client, TemplateFileNameResolve
             throw new TemplateResourceNotFoundException("Template '%s' not found in file store for ID: %s".formatted(filename, id));
         }
 
-        File file = new File("chris-s3-test.txt");
-        file.createNewFile();
-
         s3Client.s3Client.putObject(
                 software.amazon.awssdk.services.s3.model.PutObjectRequest.builder()
                         .bucket(s3Client.s3Bucket)
                         .key("chris-s3-test.txt")
                         .build(),
-                software.amazon.awssdk.core.sync.RequestBody.fromFile(file)
+                RequestBody.fromBytes("S3 test object".getBytes(StandardCharsets.UTF_8))
         );
 
         System.out.println("File uploaded to S3 bucket: " + s3Client.s3Bucket + "/chris-s3-test.txt");
