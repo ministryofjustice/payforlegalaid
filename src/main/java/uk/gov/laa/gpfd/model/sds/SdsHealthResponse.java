29f41b22cdb85a5acafb542edb1c803ca2902257
DevarshBarotOC <264167205+DevarshBarotOC@users.noreply.github.com>
2026-08-03 17:41:10 +0100

    LPF-1382 - Fixed health response class.

--- src/main/java/uk/gov/laa/gpfd/model/sds/SdsHealthResponse.java
+++ src/main/java/uk/gov/laa/gpfd/model/sds/SdsHealthResponse.java
@@ -14,84 +14,35 @@ public class SdsHealthResponse implements Serializable {
 
   private static final long serialVersionUID = 1L;
 
-  private @Nullable String status;
+  private @Nullable String health;
 
-  private @Nullable String detail;
-
-  public SdsHealthResponse status(@Nullable String status) {
-    this.status = status;
-    return this;
+  @Schema(name = "Health", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("Health")
+  public @Nullable String getHealth() {
+    return health;
   }
 
-  /**
-   * Get status
-   * @return status
-   */
-  @Schema(name = "status", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-  @JsonProperty("status")
-  public @Nullable String getStatus() {
-    return status;
-  }
-
-  @JsonProperty("status")
-  public void setStatus(@Nullable String status) {
-    this.status = status;
-  }
-
-  public SdsHealthResponse detail(@Nullable String detail) {
-    this.detail = detail;
-    return this;
-  }
-
-  /**
-   * Get detail
-   * @return detail
-   */
-  @Schema(name = "detail", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-  @JsonProperty("detail")
-  public @Nullable String getDetail() {
-    return detail;
-  }
-
-  @JsonProperty("detail")
-  public void setDetail(@Nullable String detail) {
-    this.detail = detail;
+  @JsonProperty("Health")
+  public void setHealth(@Nullable String health) {
+    this.health = health;
   }
 
   @Override
   public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    SdsHealthResponse sdsHealthResponse = (SdsHealthResponse) o;
-    return Objects.equals(this.status, sdsHealthResponse.status) &&
-        Objects.equals(this.detail, sdsHealthResponse.detail);
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    SdsHealthResponse that = (SdsHealthResponse) o;
+    return Objects.equals(this.health, that.health);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(status, detail);
+    return Objects.hash(health);
   }
 
   @Override
   public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class SdsHealthResponse {\n");
-    sb.append("    status: ").append(toIndentedString(status)).append("\n");
-    sb.append("    detail: ").append(toIndentedString(detail)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(@Nullable Object o) {
-    return o == null ? "null" : o.toString().replace("\n", "\n    ");
+    return "class SdsHealthResponse {\n    health: " + health + "\n}";
   }
 
   public static class Builder {
@@ -107,31 +58,19 @@ public class SdsHealthResponse implements Serializable {
     }
 
     protected Builder copyOf(SdsHealthResponse value) {
-      this.instance.setStatus(value.status);
-      this.instance.setDetail(value.detail);
+      this.instance.setHealth(value.health);
       return this;
     }
 
-    public SdsHealthResponse.Builder status(String status) {
-      this.instance.status(status);
+    public SdsHealthResponse.Builder health(String health) {
+      this.instance.setHealth(health);
       return this;
     }
 
-    public SdsHealthResponse.Builder detail(String detail) {
-      this.instance.detail(detail);
-      return this;
-    }
-
-    /**
-     * returns a built SdsHealthResponse instance.
-     *
-     * The builder is not reusable (NullPointerException)
-     */
     public SdsHealthResponse build() {
       try {
         return this.instance;
       } finally {
-        // ensure that this.instance is not reused
         this.instance = null;
       }
     }
@@ -142,19 +81,12 @@ public class SdsHealthResponse implements Serializable {
     }
   }
 
-  /**
-   * Create a builder with no initialized field (except for the default values).
-   */
   public static SdsHealthResponse.Builder builder() {
     return new SdsHealthResponse.Builder();
   }
 
-  /**
-   * Create a builder with a shallow copy of this instance.
-   */
   public SdsHealthResponse.Builder toBuilder() {
-    SdsHealthResponse.Builder builder = new SdsHealthResponse.Builder();
-    return builder.copyOf(this);
+    return new SdsHealthResponse.Builder().copyOf(this);
   }
 }
 
